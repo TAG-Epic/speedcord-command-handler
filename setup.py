f4e216be29ab7735d1c0f056ab21913b4efe4cb2
Tag-Epic <tagepicuwu@gmail.com>
2021-04-25 18:42:56 +0200

    Fix: Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="speedian",
-    version="1.1.0",
+    version="1.1.1",
     description="A speedcord command handler",
     long_description="A speedcord command handler",
     author="tag-epic",
